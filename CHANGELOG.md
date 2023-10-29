9b9a547de817b89d61d0725953423d889a8f94ad
Team 0x6861636B <95329795+proplayer919@users.noreply.github.com>
2023-10-29 13:02:30 +1100

    Version 0.0.2-alpha-stable

---
+++ CHANGELOG.md
@@ -0,0 +1,34 @@
+# Spark Changelog
+
+This file is the changelog for Spark.
+
+## Version 0.0.1-alpha-stable
+
+### Added in v0.0.1-alpha-stable
+
+* Printing to the console
+* Setting variables
+* Reading & Writing to files
+
+### Changed in v0.0.1-alpha-stable
+
+Nothing.
+
+### Removed in v0.0.1-alpha-stable
+
+Nothing.
+
+## Version 0.0.2-alpha-stable
+
+### Added in v0.0.2-alpha-stable
+
+* Deleting files
+
+### Changed in v0.0.2-alpha-stable
+
+* File reading keyword changed to '#read_file'
+* File writing keyword changed to '#write_file'
+
+### Removed in v0.0.2-alpha-stable
+
+Nothing.
--- src/interpreter.py
+++ src/interpreter.py
@@ -7,6 +7,8 @@
 # If the license was not distributed with this code you can find it at https://fifly.org/FRSL/1.0.
 
 
+import os
+
 vars = {}
 
 def throw_error(error_message: str):
@@ -45,12 +47,12 @@ def interpret(code: str):
                 set_var_value(tokens[1], get_var_value(tokens[1][1:]))
             else:
                 set_var_value(tokens[1], tokens[2].replace("\\", " "))
-        elif tokens[0] == "#read":
+        elif tokens[0] == "#read_file":
             if tokens[1].startswith("$"):
                 set_var_value(tokens[2], open(get_var_value(tokens[1][1:]), "r").read())
             else:
                 set_var_value(tokens[2], open(tokens[1].replace("\\", " "), "r").read())
-        elif tokens[0] == "#write":
+        elif tokens[0] == "#write_file":
             if tokens[1].startswith("$"):
                 if tokens[2].startswith("$"):
                     set_var_value(tokens[2], open(get_var_value(tokens[1][1:]), "w").write(get_var_value(tokens[2][1:])))
@@ -61,5 +63,16 @@ def interpret(code: str):
                     set_var_value(tokens[2], open(tokens[1], "w").write(get_var_value(tokens[2][1:])))
                 else:
                     set_var_value(tokens[2], open(tokens[1], "w").write(tokens[2]))
+        elif tokens[0] == "#delete_file":
+            if tokens[1].startswith("$"):
+                if os.path.exists(get_var_value(tokens[1][1:])):
+                    os.remove(get_var_value(tokens[1][1:]))
+                else:
+                    throw_error("File " + get_var_value(tokens[1][1:]) + " does not exist.")
+            else:
+                if os.path.exists(tokens[1]):
+                    os.remove(tokens[1])
+                else:
+                    throw_error("File " + tokens[1] + " does not exist.")
         else:
             throw_error("Unexpected keyword \"" + tokens[0] + "\".")
