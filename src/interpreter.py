bae59ad471e49cd43b7bfcc15260cb06a16eb901
Team 0x6861636B <95329795+proplayer919@users.noreply.github.com>
2023-10-29 17:32:55 +1100

    cleanup and add parseArg function

--- src/interpreter.py
+++ src/interpreter.py
@@ -13,22 +13,26 @@ import requests
 vars = {}
 imports = []
 
+# Throws an error
 def throw_error(error_message: str):
     print(error_message)
     exit(1)
-    
+
+# Gets a variable's value
 def get_var_value(var_name: str):
     try:
         return vars[var_name]
     except:
         throw_error("VariableError: Variable " + var_name + " does not exist.")
 
+# Sets a variable's value, creating one if it doesn't exist
 def set_var_value(var_name: str, var_value: str):
     try:
         vars[var_name] = var_value
     except:
         throw_error("VariableError: Could not set value of " + var_name)
 
+# Preforms a GET request
 def get_request(url: str):
     try:
         # Send a GET request to the specified URL
@@ -44,72 +48,56 @@ def get_request(url: str):
     except Exception as e:
         # Handle exceptions (e.g., request errors, network errors)
         return f"An error occurred: {e}"
+    
+def parseArg(arg: str):
+    if arg.startswith("$"):
+        return get_var_value(arg[1:])
+    
+    arg = arg.replace("\\", " ")
+
+    if arg.startswith("&input-"):
+        return input(arg[7:])
+    else:
+        return arg
 
 def interpret(code: str):
     codelines = code.split("\n")
     for line in codelines:
         line = line.strip()
 
-        if line.startswith("//") or line.strip() == "":
+        if line.startswith("//") or line == "":
             continue
 
         tokens = line.split()
         
         if tokens[0] == "print":
-            if tokens[1].startswith("$"):
-                print(get_var_value(tokens[1][1:]))
-            else:
-                print(tokens[1].replace("\\", " "))
+            print(parseArg(tokens[1]))
         elif tokens[0] == "set":
-            if tokens[2].startswith("$"):
-                set_var_value(tokens[1], get_var_value(tokens[1][1:]))
-            else:
-                set_var_value(tokens[1], tokens[2].replace("\\", " "))
+            set_var_value(tokens[1], parseArg(tokens[2]))
         elif tokens[0] == "#read_file":
             if imports.count("filesystem") == 0:
                 throw_error("You cannot use file operation without importing the 'filesystem' module.")
                 
-            if tokens[1].startswith("$"):
-                set_var_value(tokens[2], open(get_var_value(tokens[1][1:]), "r").read())
-            else:
-                set_var_value(tokens[2], open(tokens[1].replace("\\", " "), "r").read())
+            set_var_value(tokens[2], open(parseArg(tokens[1]), "r").read())
         elif tokens[0] == "#write_file":
             if imports.count("filesystem") == 0:
                 throw_error("You cannot use file operation without importing the 'filesystem' module.")
                 
-            if tokens[1].startswith("$"):
-                if tokens[2].startswith("$"):
-                    set_var_value(tokens[2], open(get_var_value(tokens[1][1:]), "w").write(get_var_value(tokens[2][1:])))
-                else:
-                    set_var_value(tokens[2], open(get_var_value(tokens[1][1:]), "w").write(tokens[2]))
-            else:
-                if tokens[2].startswith("$"):
-                    set_var_value(tokens[2], open(tokens[1], "w").write(get_var_value(tokens[2][1:])))
-                else:
-                    set_var_value(tokens[2], open(tokens[1], "w").write(tokens[2]))
+            open(parseArg(tokens[1]), "w").write(parseArg(tokens[2]))
         elif tokens[0] == "#delete_file":
             if imports.count("filesystem") == 0:
                 throw_error("You cannot use file operation without importing the 'filesystem' module.")
                 
-            if tokens[1].startswith("$"):
-                if os.path.exists(get_var_value(tokens[1][1:])):
-                    os.remove(get_var_value(tokens[1][1:]))
-                else:
-                    throw_error("File " + get_var_value(tokens[1][1:]) + " does not exist.")
+            if os.path.exists(tokens[1]):
+                    os.remove(parseArg(tokens[1]))
             else:
-                if os.path.exists(tokens[1]):
-                    os.remove(tokens[1])
-                else:
-                    throw_error("File " + tokens[1] + " does not exist.")
+                throw_error("File " + tokens[1] + " does not exist.")
         elif tokens[0] == "import":
-            imports.append(tokens[1])
+            imports.append(parseArg(tokens[1]))
         elif tokens[0] == "#http_get":
             if imports.count("http") == 0:
                 throw_error("You cannot use HTTP requests without importing the 'http' module.")
             
-            if tokens[1].startswith("$"):
-                set_var_value(tokens[2], get_request(get_var_value(tokens[1][1:])))
-            else:
-                set_var_value(tokens[2], get_request(tokens[1]))
+            set_var_value(tokens[2], get_request(parseArg(tokens[1])))
         else:
             throw_error("Unexpected keyword \"" + tokens[0] + "\".")
